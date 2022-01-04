06545e43eca8c7b259d39c4c580a373e166a6d8a
miczoc98 <miczcoc98@gmail.com>
2022-01-04 05:18:19 +0100

    refactor

---
+++ scenes/title_screen/TitleScreen.gd
@@ -0,0 +1,4 @@
+extends CanvasLayer
+
+func _on_start_button_up():
+	get_tree().change_scene("res://scenes/stage1/Stage1.tscn")
--- title_screen/TitleScreen.gd
+++
@@ -1,4 +0,0 @@
-extends CanvasLayer
-
-func _on_start_button_up():
-	get_tree().change_scene("res://environment/Environment.tscn")
